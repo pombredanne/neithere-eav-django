5872c09a27bc6c558d309182b93c4f194ffdd5a0
buremba <emrekabakci@gmail.com>
2013-09-20 13:36:46 +0000

    BaseEntity need generic attrs field to be able to function.

--- examples/grocery_shop/grocery/models.py
+++ examples/grocery_shop/grocery/models.py
@@ -4,10 +4,14 @@ from django.db import models
 # eav
 from eav.models import BaseChoice, BaseEntity, BaseSchema, BaseAttribute
 
-
+class Attribute(BaseAttribute):
+    schema = models.ForeignKey(Schema, related_name='attrs')
+    choice = models.ForeignKey(Choice, blank=True, null=True)
+    
 class Fruit(BaseEntity):
     title = models.CharField(max_length=50)
-
+    attrs = generic.GenericRelation(Attribute, object_id_field='entity_id',
+                                    content_type_field='entity_type')
     @classmethod
     def get_schemata_for_model(self):
         return Schema.objects.all()
@@ -22,10 +26,4 @@ class Schema(BaseSchema):
 
 class Choice(BaseChoice):
     schema = models.ForeignKey(Schema, related_name='choices')
-
-
-class Attribute(BaseAttribute):
-    schema = models.ForeignKey(Schema, related_name='attrs')
-    choice = models.ForeignKey(Choice, blank=True, null=True)
-
 
