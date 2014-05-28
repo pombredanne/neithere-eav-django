48aa9f0c992d65c0ce0050ed62e4e33cbf2de96f
Jon Atkinson <jon@wearefarm.com>
2014-05-28 16:00:17 +0000

    Updated urls.py to be compatible with Django 1.6.5. Removed some redundant lines which were commented out anyway.

--- examples/grocery_shop/urls.py
+++ examples/grocery_shop/urls.py
@@ -1,17 +1,10 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns, include
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^example_project/', include('example_project.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
 )
