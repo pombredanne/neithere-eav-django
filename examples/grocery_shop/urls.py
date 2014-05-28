c304d6016fd46a4398c175ddee96f67c9c633794
Jon Atkinson <jon@wearefarm.com>
2014-05-28 16:05:46 +0000

    Added staticfile.urls include directive to example.urls. This makes the admin interface display correctly with Django 1.6.5.

--- examples/grocery_shop/urls.py
+++ examples/grocery_shop/urls.py
@@ -7,4 +7,5 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
+    (r'', include('django.contrib.staticfiles.urls')),
 )
