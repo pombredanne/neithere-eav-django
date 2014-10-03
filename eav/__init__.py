2919da3c2306497302fb426cf788d5b15efcd371
Andrey Mikhaylenko <neithere@gmail.com>
2014-10-04 03:29:30 +0600

    Add contributor, bump version

--- eav/__init__.py
+++ eav/__init__.py
@@ -18,4 +18,4 @@
 #    You should have received a copy of the GNU Lesser General Public License
 #    along with EAV-Django.  If not, see <http://gnu.org/licenses/>.
 
-__version__ = '1.4.6'
+__version__ = '1.4.7'
